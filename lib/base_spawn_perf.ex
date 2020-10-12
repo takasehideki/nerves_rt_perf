2912b957905bf94e048df82aed69507151f388f6
takasehideki <takase@i.kyoto-u.ac.jp>
2020-10-12 19:59:06 +0900

    benchmark for spawn

--- lib/base_spawn_perf.ex
+++ lib/base_spawn_perf.ex
@@ -1,4 +1,4 @@
-defmodule BasePerf do
+defmodule BaseSpawnPerf do
   # macro setting for const value (defined by NervesRtPerf)
   require NervesRtPerf
   @eval_loop_num NervesRtPerf.eval_loop_num()
@@ -22,38 +22,22 @@ defmodule BasePerf do
 
     File.write(filepath, "count,time,heap_size,minor_gcs\r\n")
 
-    # generate process for output of measurement logs
-    pid = spawn(NervesRtPerf, :output, [self(), filepath, ""])
+    # generate process for evaluation
+    pid_output = spawn(NervesRtPerf, :output, [filepath, ""])
 
     case param do
       "normal" ->
-        Process.spawn(__MODULE__, :eval_loop, [0, pid], [])
+        Process.spawn(__MODULE__, :eval_loop, [0, pid_output], [])
 
       _ ->
         IO.puts("Argument error")
     end
   end
 
-  # loop for evaluation
-  def eval_loop(count, pid) do
-    case count do
-      # write results to the log file
-      n when n > @eval_loop_num ->
-        send(pid, {:ok})
-        IO.puts("Evaluation end:" <> Time.to_string(Time.utc_now()))
-        :ok
-
-      0 ->
-        IO.puts("Evaluation start:" <> Time.to_string(Time.utc_now()))
-        # ignore evaluation for the first time to avoid cache influence
-        NervesRtPerf.sum(@sum_num)
-        NervesRtPerf.fib(@fib_num)
-        :timer.sleep(5)
-        eval_loop(count + 1, pid)
-
-      _ ->
+  def eval_spawn(pid_output) do
+    receive do
+      {:ok, count} ->
         # measurement point
-        # {eval, _} = :timer.tc(NervesRtPerf, :fib, [])
         t1 = :erlang.monotonic_time()
         NervesRtPerf.sum(@sum_num)
         NervesRtPerf.fib(@fib_num)
@@ -66,11 +50,36 @@ defmodule BasePerf do
           }\r\n"
 
         # send measurement result to output process
-        send(pid, {:ok, result})
-        # sleep to wait output process
+        send(pid_output, {:ok, result})
+
+        eval_spawn(pid_output)
+    end
+
+  end
+
+  # loop for evaluation
+  def eval_loop(count, pid_output) do
+    case count do
+      # write results to the log file
+      n when n > @eval_loop_num ->
+        send(pid_output, {:ok})
+        IO.puts("Evaluation end:" <> Time.to_string(Time.utc_now()))
+        :ok
+
+      0 ->
+        IO.puts("Evaluation start:" <> Time.to_string(Time.utc_now()))
+        # ignore evaluation for the first time to avoid cache influence
+        pid_spawn = spawn(__MODULE__, :eval_spawn, [pid_output])
+        send(pid_spawn, {:ok, count})
+        :timer.sleep(5)
+        eval_loop(count + 1, pid_output)
+
+      _ ->
+        pid_spawn = spawn(__MODULE__, :eval_spawn, [pid_output])
+        send(pid_spawn, {:ok, count})
         :timer.sleep(5)
 
-        eval_loop(count + 1, pid)
+        eval_loop(count + 1, pid_output)
     end
   end
 end
