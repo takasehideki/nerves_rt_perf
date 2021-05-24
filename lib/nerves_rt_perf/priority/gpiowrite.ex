7407f508149b1691bf942e4aa61729713c2d5700
git-westriver <westriver.naoki20000316@gmail.com>
2021-05-24 15:03:24 +0900

    update gpiowrite

--- lib/nerves_rt_perf/priority/gpiowrite.ex
+++ lib/nerves_rt_perf/priority/gpiowrite.ex
@@ -23,7 +23,7 @@ defmodule NervesRtPerf.Priority.Gpiowrite do
     filepath = "/tmp/" <> filename <> ".csv"
     IO.puts("result log file: " <> filepath)
 
-    File.write(filepath, "count,time_for_write_1,time_for_write_0,heap_size,minor_gcs\r\n")
+    File.write(filepath, "count,time_for_0,time_for_1,time_for_0to1,time_for_1to0,heap_size,minor_gcs\r\n")
 
     # generate process for output of measurement logs
     pid = spawn(NervesRtPerf, :output, [filepath, ""])
@@ -48,44 +48,80 @@ defmodule NervesRtPerf.Priority.Gpiowrite do
     # sleep on each iteration
     :timer.sleep(5)
 
-    # open the pin
-    {pin_num, _} = Integer.parse(@gpio_pin)
-    {:ok, gpio} = GPIO.open(pin_num, :output)
-
     case count do
       # write results to the log file
       n when n > @eval_loop_num ->
         send(pid, {:ok})
         IO.puts("Evaluation end:" <> Time.to_string(Time.utc_now()))
-        GPIO.close(gpio)
         :ok
 
       0 ->
         IO.puts("Evaluation start:" <> Time.to_string(Time.utc_now()))
         # ignore evaluation for the first time to avoid cache influence
+        # open the pin
+        {pin_num, _} = Integer.parse(@gpio_pin)
+        {:ok, gpio} = GPIO.open(pin_num, :output)
+        # write 1
         GPIO.write(gpio, 1)
         :timer.sleep(5)
+        # rewrite 1 to 0
         GPIO.write(gpio, 0)
         :timer.sleep(5)
+        GPIO.close(gpio)
+
+        # open the pin
+        {pin_num, _} = Integer.parse(@gpio_pin)
+        {:ok, gpio} = GPIO.open(pin_num, :output)
+        # write 0
+        GPIO.write(gpio, 0)
+        :timer.sleep(5)
+        # rewrite 0 to 1
+        GPIO.write(gpio, 1)
+        :timer.sleep(5)
+        GPIO.close(gpio)
         eval_loop(count + 1, pid)
 
       _ ->
         # measurement point
+        # open the pin
+        {pin_num, _} = Integer.parse(@gpio_pin)
+        {:ok, gpio} = GPIO.open(pin_num, :output)
+        # write 1
         t1 = :erlang.monotonic_time()
         GPIO.write(gpio, 1)
         t2 = :erlang.monotonic_time()
-        GPIO.write(gpio, 0)
+        :timer.sleep(5)
+        # 1 to 0
         t3 = :erlang.monotonic_time()
+        GPIO.write(gpio, 0)
+        t4 = :erlang.monotonic_time()
+        :timer.sleep(5)
+        GPIO.close(gpio)
+
+        # open the pin
+        {pin_num, _} = Integer.parse(@gpio_pin)
+        {:ok, gpio} = GPIO.open(pin_num, :output)
+        # write 0
+        t5 = :erlang.monotonic_time()
+        GPIO.write(gpio, 0)
+        t6 = :erlang.monotonic_time()
+        :timer.sleep(5)
+        # 0 to 1
+        t7 = :erlang.monotonic_time()
+        GPIO.write(gpio, 1)
+        t8 = :erlang.monotonic_time()
+        :timer.sleep(5)
+        GPIO.close(gpio)
+
+        time_0 = :erlang.convert_time_unit(t6 - t5, :native, :microsecond)
         time_1 = :erlang.convert_time_unit(t2 - t1, :native, :microsecond)
-        time_0 = :erlang.convert_time_unit(t3 - t2, :native, :microsecond)
+        time_1to0 = :erlang.convert_time_unit(t4 - t3, :native, :microsecond)
+        time_0to1 = :erlang.convert_time_unit(t8 - t7, :native, :microsecond)
 
         result =
-          "#{count},#{time_1},#{time_0},#{Process.info(self())[:heap_size]},#{
+          "#{count},#{time_0},#{time_1},#{time_0to1},#{time_1to0},#{Process.info(self())[:heap_size]},#{
             Process.info(self())[:garbage_collection][:minor_gcs]
           }\r\n"
-
-        # close gpio pin
-        GPIO.close(gpio)
 
         # send measurement result to output process
         send(pid, {:ok, result})
